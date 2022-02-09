93583659bb818db9fad148daa370f4e6a50e2c06
MakPM <pmmakarella@gmail.com>
2022-02-09 21:39:46 +0200

    CRUD's implemented

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
-const res = require('express/lib/response');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 
 main().then(console.log('connected')).catch(err => console.log(err));
@@ -13,6 +13,8 @@ async function main() {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
     res.render('dummy');
@@ -23,9 +25,34 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', {allCamps});
 });
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/newcamp');
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const camp = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {camp});
+});
+
 app.get('/campgrounds/:id', async (req, res) => {
     const camp = await Campground.findById(req.params.id);
     res.render('campgrounds/show', {camp});
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const newCamp = new Campground(req.body.campground);
+    await newCamp.save();
+    res.redirect(`campgrounds/${newCamp._id}`);
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const camp = await Campground.findByIdAndUpdate(req.params.id, {...req.body.campground});
+    res.redirect(`/campgrounds/${camp._id}`);
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    const camp = await Campground.findByIdAndDelete(req.params.id);
+    res.redirect(`/campgrounds`);
 });
 
 app.listen(3000, () => {
